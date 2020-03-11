c33a02151eda01f05175e3c0b19e8a731a679351
Janis Saudins <saudins@gmail.com>
2020-03-11 19:27:22 +0200

    Upload from computer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoItem = ({text}) => (
+  <li>{text}</li>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo: ''});
+  }
+  render() {
+    const {newTodo} = this.state;
+    const todos = this.state.todos.map((t, i) => (
+      <TodoItem key={i} text={t} />
+    ));
+    return (
+      <div className="App">
+        <h1>Simple Todo App</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="todo-input"
+            autoComplete="off"
+            type="text"
+            name="newTodo"
+            placeholder="What needs to be done?"
+            value={newTodo}
+            onChange={(e) => this.setState({[e.target.name]: e.target.value })}
+          />
+          <button
+            type="submit"
+            className="save-button"
+          >
+            SAVE
+          </button>
+        </form>
+        <div className="todo-content">
+          <ol>
+            {todos}
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
